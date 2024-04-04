f5a19fb5564d64f6b1f78797ac0f8c11468dcfa2
gowry lakshmi <sgowrylakshmi@gmail.com>
2024-04-04 23:32:56 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  TextInput,
+  FlatList,
+} from "react-native";
+import { useState, useEffect } from "react";
+import HTML from "react-native-render-html";
 
 export default function App() {
+  const [postList, setPostList] = useState([]);
+  const fetchData = async () => {
+    try {
+      const query = "blisters";
+      const response = await fetch(`https://api.nhs.uk/conditions/${query}`, {
+        headers: {
+          "subscription-key": "13a43248355e41c0beedcd663fc14c2b",
+        },
+      });
+      //const data = await response.json();
+      const result = await response.json();
+      const data = result.mainEntityOfPage;
+      const headlines = data.map((el) => el.headline);
+      const data1 = data.map((el) => el.mainEntityOfPage);
+      const mappedData = data1.map((el) => {
+        if (Array.isArray(el)) {
+          return el.map((obj) => ({
+            text: obj.text,
+          }));
+        } else {
+          return el;
+        }
+      });
+      const mappedResults = headlines.map((headline, index) => ({
+        headline: headline,
+        text: mappedData[index][0].text,
+      }));
+      //console.log(mappedResults);
+      setPostList(mappedResults);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  console.log(postList);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const [name, setName] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={setName}
+        placeholder="What is your Emergency?"
+      />
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.titleText}>{item.headline}</Text>
+
+                {<HTML source={{ html: item.text }} />}
+              </View>
+            );
+          }}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: StatusBar.currentHeight,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+  },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "white",
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 30,
+  },
+  bodyText: {
+    fontSize: 24,
+    color: "#666666",
   },
 });
